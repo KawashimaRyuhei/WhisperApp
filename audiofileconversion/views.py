617bea85155ca691ab950b5b97587e9a8f74f898
kawashima <11011011wxy@gmail.com>
2023-10-31 23:39:21 +0900

    replace 汎用ﾋﾞｭｰ views.py

--- audiofileconversion/views.py
+++ audiofileconversion/views.py
@@ -1,23 +1,44 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import HttpResponse, HttpResponseRedirect
+
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("audiofileconversion/index.html")
-    context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
+class IndexView(generic.ListView):
+    template_name = "audiofileconversion/index.html"
+    context_object_name = "latest_question_list"
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "audiofileconversion/detail.html", {"question": question})
+    def get_queryset(self):
+        """ Return the last five published questions. """
+        return Question.objects.order_by("pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "audiofileconversion/detail.html"
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "audiofileconversion/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "audiofileconversion/results.html"
+
+
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     template = loader.get_template("audiofileconversion/index.html")
+#     context = {"latest_question_list": latest_question_list}
+#     return HttpResponse(template.render(context, request))
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "audiofileconversion/detail.html", {"question": question})
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "audiofileconversion/results.html", {"question": question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)

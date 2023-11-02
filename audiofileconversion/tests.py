0ce8392f3c0b50bbbeafc414b18ba089f1d56974
kawashima <11011011wxy@gmail.com>
2023-11-03 00:46:19 +0900

    fix tests.py

--- audiofileconversion/tests.py
+++ audiofileconversion/tests.py
@@ -7,9 +7,9 @@ from .models import Question
 
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
-        time = timezone.now()
+        time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
-        self.assertIs(future_question.was_published_recently, False)
+        self.assertIs(future_question.was_published_recently(), False)
 
 
 # Create your tests here.

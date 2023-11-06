632ffe1e95ae8af300f7021acef7bfb70152837a
kawashima <11011011wxy@gmail.com>
2023-11-07 00:30:35 +0900

    update index.html

--- audiofileconversion/tests.py
+++ audiofileconversion/tests.py
@@ -66,7 +66,7 @@ class QuestionIndexViewTests(TestCase):
             [question2, question1],
         )
 
-class QuestionDetailViewTest(TestCase):
+class QuestionDetailViewTests(TestCase):
     def test_future_question(self):
         future_question = create_question(question_text="Future question.", days=5)
         url = reverse("audiofileconversion:detail", args=(future_question.id,))

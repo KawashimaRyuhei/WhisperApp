0b34627e2b725ed6326f05fea6baf356b934da3a
kawashima <11011011wxy@gmail.com>
2023-11-10 23:48:34 +0900

    update admin.py

--- audiofileconversion/admin.py
+++ audiofileconversion/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -11,7 +11,7 @@ class QuestionAdmin(admin.ModelAdmin):
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
-    inline = [ChoiceInline]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
 
